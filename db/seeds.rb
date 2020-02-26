b48413e17428898d93515cbbd00b6a920dff0d51
Rich Cherry <rich.m.cherry@gmail.com>
2020-02-26 17:31:11 +0800

    Completed exercise, added styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Motion Cafe',
+    address:      '4 Badu Bolong St, Canggu, Bali',
+    phone_number: '234-42343',
+    category:     'belgian',
+  },
+  {
+    name:         'The Loft',
+    address:      '4 Badu Bolong St, Canggu, Bali',
+    phone_number: '234-42343',
+    category:     'japanese',
+  },
+  {
+    name:         'Dishoom',
+    address:      '4 Badu Bolong St, Canggu, Bali',
+    phone_number: '234-42343',
+    category:     'chinese',
+  },
+  {
+    name:         'Milu',
+    address:      '4 Badu Bolong St, Canggu, Bali',
+    phone_number: '234-42343',
+    category:     'italian',
+  },
+  {
+    name:         'Pizza Cafe',
+    address:      '4 Badu Bolong St, Canggu, Bali',
+    phone_number: '234-42343',
+    category:     'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
